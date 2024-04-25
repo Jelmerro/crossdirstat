78a331eda65ee986c3ed944de8214c94ec4afcac
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2024-04-25 21:11:44 +0200

    update deps

--- build.js
+++ build.js
@@ -1,19 +1,36 @@
 "use strict"
 
 const builder = require("electron-builder")
-const rimraf = require("rimraf").sync
-const builds = {}
-
-rimraf("dist/")
+const {rmSync, readdir, unlinkSync} = require("fs")
+const ebuilder = {"config": {
+    /**
+     * Remove all locales except English US from the build.
+     * @param {import("electron-builder").AfterPackContext} context
+     */
+    "afterPack": context => {
+        const localeDir = `${context.appOutDir}/locales/`
+        readdir(localeDir, (_err, files) => {
+            files?.filter(f => !f.match(/en-US\.pak/))
+                .forEach(f => unlinkSync(localeDir + f))
+        })
+    }
+}}
+rmSync("dist/", {"force": true, "recursive": true})
 process.argv.slice(1).forEach(a => {
-    if (a === "--linux") {
-        builds.linux = []
+    if (a === "--help") {
+        console.info("Basic crossdirstat build script, these are its options:")
+        console.info(" --all, --linux, --win, --mac")
+        console.info("By default it will only build for the current platform.")
+        process.exit(0)
     }
-    if (a === "--win") {
-        builds.win = []
+    if (a === "--linux" || a === "--all") {
+        ebuilder.linux = []
     }
-    if (a === "--mac") {
-        builds.mac = []
+    if (a === "--win" || a === "--all") {
+        ebuilder.win = []
+    }
+    if (a === "--mac" || a === "--all") {
+        ebuilder.mac = []
     }
 })
-builder.build(builds).then(e => console.info(e)).catch(e => console.error(e))
+builder.build(ebuilder).then(e => console.info(e)).catch(e => console.error(e))
