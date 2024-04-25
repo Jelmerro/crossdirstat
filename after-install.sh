e3476f1b868876cb80bb33a91509b0146b5dcde2
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2024-04-25 21:43:46 +0200

    update build config

---
+++ after-install.sh
@@ -0,0 +1,4 @@
+ln -sf /opt/Crossdirstat/crossdirstat /usr/bin/crossdirstat
+chmod 4755 '/opt/Crossdirstat/chrome-sandbox' || true
+update-mime-database /usr/share/mime || true
+update-desktop-database /usr/share/applications || true
