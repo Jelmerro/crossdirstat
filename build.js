88801345cebcf7b2b3bcb97f9d5a1d16e625ae0f
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2024-11-07 22:06:21 +0100

    move builder config to script

--- build.js
+++ build.js
@@ -12,9 +12,64 @@ const ebuilder = {"config": {
             files?.filter(f => !f.match(/en-US\.pak/))
                 .forEach(f => unlinkSync(localeDir + f))
         })
+    },
+    "appId": "com.github.Jelmerro.crossdirstat",
+    "copyright": "Copyright @ Jelmer van Arnhem | "
+        + "Licensed as free software (GPL-3.0 or later)",
+    "deb": {
+        "afterInstall": "./after-install.sh",
+        "fpm": ["--after-upgrade=./after-install.sh"]
+    },
+    "linux": {
+        "category": "Office;Utility;",
+        "executableArgs": ["--ozone-platform-hint=auto"],
+        "executableName": "crossdirstat",
+        "icon": "app/icons",
+        "maintainer": "Jelmer van Arnhem",
+        "publish": null,
+        "target": [
+            {"arch": ["arm64", "x64"], "target": "AppImage"},
+            {"arch": ["arm64", "x64"], "target": "deb"},
+            {"arch": ["arm64", "x64"], "target": "pacman"},
+            {"arch": ["arm64", "x64"], "target": "rpm"},
+            {"arch": ["x64"], "target": "snap"},
+            {"arch": ["arm64", "x64"], "target": "tar.gz"}
+        ]
+    },
+    "mac": {
+        "category": "public.app-category.utilities",
+        "icon": "app/icons",
+        "publish": null,
+        "target": [
+            {"arch": ["arm64", "x64"], "target": "zip"}
+        ]
+    },
+    "nsis": {
+        "differentialPackage": false,
+        "license": "LICENSE",
+        "oneClick": false
+    },
+    "productName": "Crossdirstat",
+    "rpm": {
+        "afterInstall": "./after-install.sh",
+        "fpm": [
+            "--rpm-rpmbuild-define=_build_id_links none",
+            "--after-upgrade=./after-install.sh"
+        ]
+    },
+    "win": {
+        "icon": "app/icons/512x512.png",
+        "legalTrademarks": "Copyright @ Jelmer van Arnhem | "
+            + "Licensed as free software (GPL-3.0 or later)",
+        "publish": null,
+        "target": [
+            {"arch": ["x64"], "target": "nsis"},
+            {"arch": ["x64"], "target": "portable"},
+            {"arch": ["arm64", "x64"], "target": "zip"}
+        ]
     }
 }}
-process.argv.slice(1).forEach(a => {
+process.argv.slice(2).forEach(a => {
     if (a === "--help") {
         console.info("Basic crossdirstat build script, these are its options:")
         console.info(" --all, --linux, --win, --mac")
